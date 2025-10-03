17234b934c60e156f740f579ba9db61af4570197
faithfulcoronel <39255507+faithfulcoronel@users.noreply.github.com>
2025-10-03 21:35:09 +0800

    Fix Hilt aggregated deps failure by forcing Javapoet version

--- medialert_project/build.gradle.kts
+++ medialert_project/build.gradle.kts
@@ -2,3 +2,14 @@
 plugins {
     alias(libs.plugins.android.application) apply false
 }
+
+subprojects {
+    configurations.configureEach {
+        resolutionStrategy.eachDependency {
+            if (requested.group == "com.squareup" && requested.name == "javapoet") {
+                useVersion(libs.versions.javapoet.get())
+                because("Older JavaPoet versions miss ClassName.canonicalName used by recent tooling")
+            }
+        }
+    }
+}
