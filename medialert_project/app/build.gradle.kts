e7403c81e34f3a139e459a3909741f4390e22b06
faithfulcoronel <39255507+faithfulcoronel@users.noreply.github.com>
2025-10-03 21:40:34 +0800

    fix

--- medialert_project/app/build.gradle.kts
+++ medialert_project/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.kapt)
-    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.hilt.android) apply false
 }
 
 android {
